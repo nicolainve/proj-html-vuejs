9562c14d554febb64db3551803626bf4e28352bb
Nicola <nginvernizzi1@gmail.com>
2020-11-30 15:49:52 +0100

    initial navbar header with vue

--- js/main.js
+++ js/main.js
@@ -5,6 +5,37 @@
 const app = new Vue({
     el: '#app',
     data: {
-        
+        navbarHeader: [
+            {
+                text: 'Home',
+                dropdown: 'fas fa-angle-down',
+            },
+            {
+                text: 'Pages',
+                dropdown: 'fas fa-angle-down',
+            },
+            {
+                text: 'Courses',
+                dropdown: 'fas fa-angle-down',
+            },
+            {
+                text: 'Features',
+                dropdown: 'fas fa-angle-down',
+            },
+            {
+                text: 'Blog',
+                dropdown: 'fas fa-angle-down',
+            },
+            {
+                text: 'Shop',
+                dropdown: 'fas fa-angle-down',
+            },
+            {
+                class: 'fas fa-user-circle',
+            },
+            {
+                class: 'fas fa-shopping-cart',
+            }
+            ]
     }
 });
